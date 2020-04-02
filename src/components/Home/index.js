5f031f6641e0c36540d45f3d0766ea31aad183d5
Vladislav Veluga <vladislav805@yandex.com>
2020-04-02 14:56:46 +0300

    Implement redux caching for public repositories list

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,32 +3,36 @@ import './home.scss';
 import LoadableContent from '../LoadableContent';
 import RepositoryList from '../RepositoryList';
 import { fetchListPublicRepositories } from '../../github';
+import { cacheRepoList } from '../../reducers/github';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 class Home extends React.Component {
-    state = {
-        items: null,
-    };
-
     componentDidMount() {
-        this.fecthListRepositories();
+        if (!this.props.items) {
+            this.fecthListRepositories();
+        }
     }
 
     fecthListRepositories = async() => {
         const items = await fetchListPublicRepositories();
 
-        this.setState({ items });
+        this.props.cacheRepoList(items);
     }
 
     render() {
         return (
             <div className="home">
                 <h1>Public repositories</h1>
-                <LoadableContent loading={!this.state.items} render={() => (
-                    <RepositoryList items={this.state.items} />
+                <LoadableContent loading={!this.props.items} render={() => (
+                    <RepositoryList items={this.props.items} />
                 )} />
             </div>
         );
     }
 }
 
-export default Home;
+const mapStateToProps = store => ({ items: store.publicRepos });
+const mapDispatchToProps = dispatch => bindActionCreators({ cacheRepoList }, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from 'redux';
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
 import App from './components/App';
-import * as reducers from './reducers';
+import githubReducer from './reducers/github';
 
 if (!process.env.GITHUB_TOKEN) {
     const str = 'Cannot find GITHUB_TOKEN. Please, make sure that you added GitHub token to .env before build/run app';
@@ -12,7 +12,7 @@ if (!process.env.GITHUB_TOKEN) {
     throw new Error(str);
 }
 
-const store = createStore(combineReducers(reducers));
+const store = createStore(githubReducer);
 
 ReactDOM.render(
     <Provider store={store}>
