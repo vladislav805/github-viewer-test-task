ca712a355fe41b93c0afb546c5b53ea03ba5c6af
Vladislav Veluga <vladislav805@yandex.com>
2020-04-09 16:56:06 +0300

    Add loading with offset

--- src/components/RepositoryList/index.js
+++ src/components/RepositoryList/index.js
@@ -3,14 +3,22 @@ import PropTypes from 'prop-types';
 import './repository-list.scss';
 import RepositoryItem from '../RepositoryItem';
 
-const RepositoryList = ({ items }) => (
+const RepositoryList = ({ items, next, nextLoading }) => (
     <div className="repository-list">
         {items.map(repository => <RepositoryItem key={repository.id} repository={repository} />)}
+        <button
+            className="repository-list--button-next"
+            disabled={nextLoading}
+            onClick={next}>
+            Next
+        </button>
     </div>
 );
 
 RepositoryList.propTypes = {
     items: PropTypes.arrayOf(PropTypes.object),
+    next: PropTypes.func.isRequired,
+    nextLoading: PropTypes.bool,
 };
 
 export default RepositoryList;
--- src/github/index.js
+++ src/github/index.js
@@ -1,13 +1,15 @@
+import qs from 'querystring';
+
 const getEndpoint = method => `https://api.github.com/${method}`;
 
 const getCustomHeader = () => process.env.GITHUB_TOKEN !== 'none' ? {
     'Authorization': 'token ' + process.env.GITHUB_TOKEN,
 } : {};
 
-const githubRequest = async(method) => fetch(getEndpoint(method), {
+const githubRequest = async(method, props = {}) => fetch(getEndpoint(method) + (props ? '?' + qs.stringify(props) : ''), {
     headers: getCustomHeader()
 }).then(res => res.json())
 
-export const fetchListPublicRepositories = async() => githubRequest('repositories');
+export const fetchListPublicRepositories = async(since) => githubRequest('repositories', since && { since });
 export const fetchRepositoryInfo = async(fullName) => githubRequest(`repos/${fullName}`);
 export const fetchRepositoryContent = async(fullName, path = '/') => githubRequest(`repos/${fullName}/contents/${path}`);
